016345235f1b8c57b82e97064ed7f53307ccf25e
Kuba <zuberzuber@gmail.com>
2022-04-19 19:56:58 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String month = "april";
+        int year = 2022;
+        long day = 19;
+        System.out.println("miesiąc:" +month);
+        System.out.println("miesiąc:" +year);
+        System.out.println("miesiąc:" +day);
+    }
+}
